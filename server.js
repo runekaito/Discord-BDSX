9c150d53311e4e3116968cd9effa502e2c297a4a
kaito02020424 <kaito02020424@gmail.com>
2023-01-23 21:46:00 +0900

    Fix:Debug時の残りを削除

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ if (config.lang === undefined || !(config.lang in { "ja": null, "en": null })) {
 } else {
     country = config.lang;
 }
-path="a";//error
 let lang = JSON.parse(fs.readFileSync(`${filepath}/lang.json`))[country];
 let info = { "version": JSON.parse(fs.readFileSync(`${filepath}/lang.json`)).version, "author": JSON.parse(fs.readFileSync(`${filepath}/lang.json`)).author }
 
