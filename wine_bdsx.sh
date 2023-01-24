0c8a4024c344346c1988a86ccdda9425ab0b817b
kaito02020424 <104062884+kaito02020424@users.noreply.github.com>
2023-01-25 07:16:45 +0900

    Fix wine_bdsx.sh

--- wine_bdsx.sh
+++ wine_bdsx.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-if [ -f ~/.wine/dosdevices/c:/windows/system32/node.exe ]; then
-wget -O ~/.wine/dosdevices/c:/windows/system32/node.exe https://nodejs.org/dist/v18.13.0/win-x64/node.exe
+if [ ! -f ~/.wine/dosdevices/c:/windows/system32/node.exe ]; then
+sudo wget -O ~/.wine/dosdevices/c:/windows/system32/node.exe https://nodejs.org/dist/v18.13.0/win-x64/node.exe
 fi
 export NODE_SKIP_PLATFORM_CHECK=1
 if [ -f ./bdsx.sh ]; then
